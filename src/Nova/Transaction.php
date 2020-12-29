77b51765d50b2ced2dc0ac1c34af4ff49670c9fd
Ismail Zare <zarehesmaiel@gmail.com>
2020-12-29 14:18:51 +0330

    Adde some new fieds

--- src/Nova/Transaction.php
+++ src/Nova/Transaction.php
@@ -4,7 +4,8 @@ namespace Armincms\Arminpay\Nova;
 
 use Illuminate\Support\Str; 
 use Illuminate\Http\Request; 
-use Laravel\Nova\Fields\{ID, Text, Number, DateTime, BelongsTo, MorphTo};   
+use Laravel\Nova\Fields\{Code, Badge, Text, Number, DateTime, BelongsTo, MorphTo};   
+use Armincms\Nova\Fields\Money;
 use Armincms\Arminpay\Helper;
 
 class Transaction extends Resource
@@ -31,7 +32,7 @@ class Transaction extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'reference_number'
+        'reference_number', 'amount'
     ]; 
 
     /**
@@ -42,10 +43,13 @@ class Transaction extends Resource
      */
     public function fields(Request $request)
     {
-        return [
-            ID::make()->sortable(),  
-
+        return [  
             Text::make(__('Reference Number'), 'reference_number')
+                ->sortable(),
+
+            Money::make(__('Amount'), 'amount')
+                ->currency($this->currency)
+                ->exceptOnForms()
                 ->sortable(), 
 
             MorphTo::make(__('Billlable'), 'billable')
@@ -54,9 +58,35 @@ class Transaction extends Resource
             BelongsTo::make(__('Gateway'), 'gateway', Gateway::class)
                 ->sortable(),
 
-            DateTime::make(__('Payment Date'), 'created_at'),
+            Badge::make(__('Status'), 'marked_as') 
+                ->map([
+                    $this->getDraftValue()  => 'info',
+                    $this->getPendingValue()=> 'info',
+                    $this->getSuccessValue()=> 'success',
+                    $this->getFailsValue()  => 'danfer', 
+                    $this->getCancellationValue() => 'warning',
+                    
+                ]),
+
+            DateTime::make(__('Payment Date'), 'created_at')
+                ->sortable(),
+
+            $this->when(! is_null($this->exception), function() {
+                return Code::make(__('Exception'), 'exception')->onlyOnDetail();
+            }),
         ];
     }   
+
+    /**
+     * Determine if the current user can create new resources.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    public static function authorizeToCreate(Request $request)
+    { 
+        return false;
+    }  
 
     /**
      * Determine if the current user can create new resources.
@@ -68,6 +98,19 @@ class Transaction extends Resource
     { 
         return false;
     }
+
+    /**
+     * Determine if the current user can update the given resource or throw an exception.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function authorizedToUpdate(Request $request)
+    {
+        return false;
+    } 
 
     /**
      * Determine if the current user can update the given resource or throw an exception.
